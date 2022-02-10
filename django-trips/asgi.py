bf234784de9033390337153cbcac0d39ca064c1b
=Awais Jibran <awaisdar001@gmail.com>
2022-02-11 00:22:23 +0500

    move to new settings package.

--- django-trips/asgi.py
+++ django-trips/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-trips.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.common')
 
 application = get_asgi_application()
--- django-trips/wsgi.py
+++ django-trips/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-trips.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.common')
 
 application = get_wsgi_application()
